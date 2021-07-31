591f115e0d365959c081e1355cfef6ffeadcb710
FelipePDS <felipepdasilva66@gmail.com>
2021-07-31 15:14:00 -0300

    style the footer

--- components/footer-component.php
+++ components/footer-component.php
@@ -8,71 +8,24 @@
                         <p>Compre produtos de StreetWear</p>
                     </div>
                 </div>
-                <div class="col-sm-7">
-                    <div class="col-sm-3">
-                        <div class="video-gallery text-center">
-                            <a href="#">
-                                <!--<div class="iframe-img">
-                                 <img src="images/home/iframe1.png" alt="" /> 
-                                </div>
-                                <div class="overlay-icon">
-                                    <i class="fa fa-play-circle-o"></i>
-                                </div>
-                            </a>
-                            <p>Circle of Hands</p>
-                            <h2>24 DEC 2014</h2> -->
-                        </div>
-                    </div> 
-                    
-                    <div class="col-sm-3">
-                        <div class="video-gallery text-center">
-                            <a href="#">
-                               <!-- <div class="iframe-img">
-                                    <img src="images/home/iframe2.png" alt="" />
-                                </div>
-                                <div class="overlay-icon">
-                                    <i class="fa fa-play-circle-o"></i>
-                                </div>
-                            </a>
-                            <p>Circle of Hands</p>
-                            <h2>24 DEC 2014</h2> -->
-                        </div>
-                    </div>
-                    
-                    <div class="col-sm-3">
-                        <div class="video-gallery text-center">
-                            <a href="#">
-                             <!--   <div class="iframe-img">
-                                    <img src="images/home/iframe3.png" alt="" />
-                                </div>
-                                <div class="overlay-icon">
-                                    <i class="fa fa-play-circle-o"></i>
-                                </div>
-                            </a>
-                            <p>Circle of Hands</p>
-                            <h2>24 DEC 2014</h2> -->
-                        </div>
-                    </div>
-                    
-                    <div class="col-sm-3">
-                        <div class="video-gallery text-center">
-                            <a href="#">
-                              <!--  <div class="iframe-img">
-                                    <img src="images/home/iframe4.png" alt="" />
-                                </div>
-                                <div class="overlay-icon">
-                                    <i class="fa fa-play-circle-o"></i>
-                                </div>
-                            </a>
-                            <p>Circle of Hands</p>
-                            <h2>24 DEC 2014</h2> -->
-                        </div>
-                    </div>
-                </div>
+
                 <div class="col-sm-3">
-                    <div class="address">
-                        <img src="images/home/map.png" alt="" />
-                        <p>1941 Av. Paulista - Bela Vista, SP (São Paulo)</p>
+                    <div class="social-networks-container">
+                        <h2>Acompanhe-nos</h2>
+
+                        <div class="social-networks-content">
+                            <a href="">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+
+                            <a href="">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+
+                            <a href="">
+                                <i class="fa fa-youtube"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -89,7 +42,6 @@
                             <li><a href="#">Ajuda online</a></li>
                             <li><a href="#">Contate-Nos</a></li>
                             <li><a href="#">Status do pedido</a></li>
-                            <!-- <li><a href="#">Change Location</a></li> -->
                             <li><a href="#">FAQ’s</a></li>
                         </ul>
                     </div>
@@ -130,13 +82,13 @@
                     </div>
                 </div> 
                 <div class="col-sm-3 col-sm-offset-1">
-                    <div class="single-widget">
-                        <h2>SOBRE O COMPRADOR</h2>
-                        <form action="#" class="searchform">
-                            <input type="text" placeholder="Seu endereço de email" />
-                            <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-                            <p>Obtenha as atualizações mais recentes de nosso site e fique atualizado ...</p>
-                        </form>
+                    <div class="single-widget payment-methods-container">
+                        <h2>Formas de Pagamento</h2>
+                        <div class="payment-methods-content">
+                            <img src="images/payment/mastercard.svg" alt="">
+                            <img src="images/payment/visa.svg" alt="">
+                            <img src="images/payment/elo.svg" alt="">
+                        </div>
                     </div>
                 </div>
                 
@@ -148,7 +100,7 @@
         <div class="container">
             <div class="row">
                 <p class="pull-left">Copyright © 2021 HypeLab Inc. Todos os direitos reservados.</p>
-                <p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+                <!-- <p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p> -->
             </div>
         </div>
     </div>

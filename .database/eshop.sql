e8d38e24c1434baf7838e12febc28f1a825b0e44
FelipePDS <felipepdasilva66@gmail.com>
2021-08-24 19:19:57 -0300

    add new field in seller table

--- .database/eshop.sql
+++ .database/eshop.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29-Maio-2021 às 03:07
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.3
+-- Tempo de geração: 25-Ago-2021 às 00:19
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -41,7 +40,9 @@ CREATE TABLE `carrinho` (
 INSERT INTO `carrinho` (`id`, `idProduto`, `idCliente`) VALUES
 (38, 12, 2),
 (39, 15, 6),
-(42, 17, 6);
+(42, 17, 6),
+(43, 14, 6),
+(44, 12, 6);
 
 -- --------------------------------------------------------
 
@@ -95,7 +96,9 @@ INSERT INTO `clientes` (`id`, `nome`, `email`, `telefone`, `senha`, `endereco`, 
 (3, 'Luis Santos', 'luiz2018@hotmail.com', '(21) 90000-1111', '123', 'Rua da minha cas', 33, 'Casa 10', '22775240', 'Barra', 'Rio de janeiro - RJ', 'Rio de janeiro', 0, NULL, NULL),
 (4, 'Guilherme Medeiros', 'luto.beibe@gmail.com', '992725796', '12345', 'Rua tal', 363, '', '09370350', 'Bairro tal', 'Maua', 'São paulo', 1, NULL, NULL),
 (5, 'Maria Silva ', 'mariasilva@gmail.com', '(11) 93583-23', '123', '', 0, '', '09320-305', 'Vila Vitória', 'Mauá', 'São Paulo', 0, 'feminino', NULL),
-(6, 'Felipe Pinto', 'felipepinto@gmail.com', '(11) 93482-49', '123', '', 0, '', '09320-305', 'Vila Vitória', 'Mauá', 'São Paulo', 0, 'masculino', '2021-05-19 22:52:18');
+(6, 'Felipe Pinto', 'felipepinto@gmail.com', '(11) 93482-49', '123', '', 0, '', '09320-305', 'Vila Vitória', 'Mauá', 'São Paulo', 1, 'masculino', '2021-05-19 22:52:18'),
+(7, 'Teste', 'teste@email.com', '(38) 94324-8234', '4535345345', '', 0, '', '34243-242', 'gfsdgsdfg', 'Mauá', 'SP', 0, 'masculino', '2021-08-24 18:40:17'),
+(8, 'Teste2', 'teste2@email.com', '(38) 94324-8234', 'fadsfsafsa', '', 0, '', '34243-242', 'gfsdgsdfg', 'Mauá', 'SP', 0, 'masculino', '2021-08-24 18:49:29');
 
 -- --------------------------------------------------------
 
@@ -119,12 +122,11 @@ CREATE TABLE `compras` (
 --
 
 INSERT INTO `compras` (`id`, `id_comprador`, `id_fatura`, `nome_produto`, `data_compra`, `status`, `detalhes`, `external_reference`) VALUES
-(11, 3, 12, 'BOLA LIGA DOS CAMPEÃ•ES DA UFA', '22/06/2019', 0, '', 'ID: 26633'),
-(16, 4, 17, 'Tênis Nike Air Max Genome Masculino', '03/05/2021', 0, '', 'ID: 39864'),
 (17, 4, 18, 'Tênis Nike Air Max Genome Masculino', '03/05/2021', 0, '', 'ID: 36169'),
 (18, 4, 19, 'Tênis Nike Air Max Genome Masculino', '03/05/2021', 0, '', 'ID: 12728'),
 (19, 4, 20, 'Tênis Nike Air Max Genome Masculino', '03/05/2021', 0, '', 'ID: 56388'),
-(20, 4, 21, 'Tênis Nike PG 5 Unissex', '03/05/2021', 0, '', 'ID: 55861');
+(21, 6, 22, 'Tênis Nike Air Max Genome Masculino', '03/08/2021', 0, '', 'ID: 52505'),
+(22, 6, 23, 'Tênis Nike PG 5 Unissex', '18/08/2021', 0, '', 'ID: 39043');
 
 -- --------------------------------------------------------
 
@@ -146,12 +148,11 @@ CREATE TABLE `faturas` (
 --
 
 INSERT INTO `faturas` (`id`, `id_cliente`, `preco`, `status`, `data`, `data_vencimento`) VALUES
-(12, 3, '1,99', 1, '22/06/2019', '27/06/2019'),
-(17, 4, '999,99', 0, '03/05/2021', '08/05/2021'),
 (18, 4, '999,99', 0, '03/05/2021', '08/05/2021'),
 (19, 4, '999,99', 0, '03/05/2021', '08/05/2021'),
 (20, 4, '999,99', 0, '03/05/2021', '08/05/2021'),
-(21, 4, '749,99', 0, '03/05/2021', '08/05/2021');
+(22, 6, '999,99', 0, '03/08/2021', '08/08/2021'),
+(23, 6, '749,99', 1, '18/08/2021', '23/08/2021');
 
 -- --------------------------------------------------------
 
@@ -176,9 +177,9 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `foto`, `tipo_fatura`, `estoque`, `preco`, `categoria`, `detalhes`, `idVendedor`) VALUES
-(12, 'Tênis Nike Air Max Genome Masculino', 'images/uploads/tenis-nike-air-max-genome-masculino-CW1648-004-1.jpg', 0, 46, '999,99', '10', 'Inspirado no visual do início dos anos 2000, o Air Max Genome reformula casa do Air Max. Seu cabedal tecnológico apresenta uma mistura de materiais, incluindo camadas sem costura, mesh arejado e detalhes em TPU durável. A unidade Air em toda a extensão, de baixo perfil acrescenta conforto para combinar com seu design elegante que com certeza se tornará seu novo Air Max favorito.', NULL),
+(12, 'Tênis Nike Air Max Genome Masculino', 'images/uploads/tenis-nike-air-max-genome-masculino-CW1648-004-1.jpg', 0, 45, '999,99', '10', 'Inspirado no visual do início dos anos 2000, o Air Max Genome reformula casa do Air Max. Seu cabedal tecnológico apresenta uma mistura de materiais, incluindo camadas sem costura, mesh arejado e detalhes em TPU durável. A unidade Air em toda a extensão, de baixo perfil acrescenta conforto para combinar com seu design elegante que com certeza se tornará seu novo Air Max favorito.', NULL),
 (13, 'Tênis Nike Air Zoom Pegasus 38 Masculino', 'images/uploads/tenis-nike-air-zoom-pegasus-38-masculino-CW7356-003-1.jpg', 0, 100, '699,99', '10', 'Seu cavalo de batalha alado está de volta. O Nike Air Zoom Pegasus 38 continua a oferecer elasticidade à sua passada, utilizando a mesma espuma ágil que seu antecessor. A tela ventilada na parte de cima combina o conforto e a durabilidade que você deseja com um ajuste mais amplo na ponta.', NULL),
-(14, 'Tênis Nike PG 5 Unissex', 'images/uploads/tenis-nike-pg-5-unissex-CW3143-400-1.jpg', 0, 59, '749,99', '10', 'Paul George tem um jogo estável e confiável sem esforço. Ele leva seu tempo, mas está sempre bem posicionado para fazer o passe extra, cortar a pista ou dar um salto. Uma evolução mais leve do 4, o PG 5 combina o perfil cano baixo ágil que o Paul gosta com amortecimento Nike Air Strobel flexível em toda a extensão. A unidade Air é costurada diretamente ao cabedal, ajudando a tornar todo o tênis mais leve e trazendo o amortecimento macio e flexível diretamente embaixo do pé—perfeito para manter PG andando com conforto.', NULL),
+(14, 'Tênis Nike PG 5 Unissex', 'images/uploads/tenis-nike-pg-5-unissex-CW3143-400-1.jpg', 0, 57, '749,99', '10', 'Paul George tem um jogo estável e confiável sem esforço. Ele leva seu tempo, mas está sempre bem posicionado para fazer o passe extra, cortar a pista ou dar um salto. Uma evolução mais leve do 4, o PG 5 combina o perfil cano baixo ágil que o Paul gosta com amortecimento Nike Air Strobel flexível em toda a extensão. A unidade Air é costurada diretamente ao cabedal, ajudando a tornar todo o tênis mais leve e trazendo o amortecimento macio e flexível diretamente embaixo do pé—perfeito para manter PG andando com conforto.', NULL),
 (15, 'Tênis NikeCourt Air Vapor Pro Masculino', 'images/uploads/tenis-nikecourt-air-vapor-pro-masculino-CZ0220-124-1.jpg', 0, 10, '899,99', '10', 'O NikeCourt Air Zoom Vapor Pro pega tudo o que você adora no NikeCourt Air Zoom Vapor X e o torna mais leve e com mais suporte. O modelo com 3 camadas coloca respirabilidade, estabilidade e durabilidade onde você mais precisa, tudo isso enquanto mantém o mínimo de peso.', NULL),
 (16, 'Tênis Jordan MA2 Masculino', 'images/uploads/tenis-jordan-ma2-masculino-CV8122-006-1.jpg', 0, 12, '849,99', '10', 'Desafie o status quo com o Jordan MA2. Fabricado com uma mistura de suede, couro de grão integral e uma variedade de tecidos, o tênis apresenta etiquetas despojadas, estampas tecnológicas e espuma com arestas cruas para oferecer um equilíbrio entre o novo e o clássico. Fáceis de calçar e descalçar e naturalmente confortáveis, os tênis são um símbolo da elegância, atitude e inovação do Jordan.', NULL),
 (17, 'Tênis Nike Air Max 95 Masculino', 'images/uploads/tenis-nike-air-max-95-masculino-CZ0191-001-1.jpg', 0, 5, '999,99', '10', 'Inspirado no corpo humano e no DNA da corrida, o Nike Air Max 95 combina conforto inacreditável com estilo impressionante. Os icônicos painéis laterais representam força com cores vibrantes, enquanto o Nike Air visível no calcanhar e no antepé amortece cada passo.', NULL);
@@ -243,8 +244,16 @@ CREATE TABLE `vendedores` (
   `cidade` varchar(40) DEFAULT NULL,
   `bairro` varchar(40) DEFAULT NULL,
   `avaliacao` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL
+  `created_at` datetime DEFAULT NULL,
+  `senha` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `vendedores`
+--
+
+INSERT INTO `vendedores` (`id`, `nome`, `email`, `telefone`, `bio`, `avatar`, `genero`, `cpf`, `cep`, `estado`, `cidade`, `bairro`, `avaliacao`, `created_at`, `senha`) VALUES
+(1, 'Teste', 'teste@email.com', '(38) 94324-8234', 'fasdasdfsaf', NULL, 'masculino', '347.482.074-93', '34243-242', 'SP', 'Mauá', 'gfsdgsdfg', NULL, '2021-08-24 19:16:09', '123');
 
 --
 -- Índices para tabelas despejadas
@@ -322,7 +331,7 @@ ALTER TABLE `vendedores`
 -- AUTO_INCREMENT de tabela `carrinho`
 --
 ALTER TABLE `carrinho`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de tabela `categorias`
@@ -334,19 +343,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de tabela `faturas`
 --
 ALTER TABLE `faturas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
@@ -370,7 +379,7 @@ ALTER TABLE `subcategorias`
 -- AUTO_INCREMENT de tabela `vendedores`
 --
 ALTER TABLE `vendedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restrições para despejos de tabelas

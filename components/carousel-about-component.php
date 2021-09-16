3393d4c718fc54fb93d57a0efe7ecab4644f86ef
FelipePDS <felipepdasilva66@gmail.com>
2021-09-15 21:10:56 -0300

    change layout

--- components/carousel-about-component.php
+++ components/carousel-about-component.php
@@ -1,24 +1,25 @@
-<div class="carousel">
-  <div>
-    <img src="https://s26.postimg.org/labezuy7d/image.jpg" alt="js" />
-  </div>
+<div class="carousel-wrapper">
+  <div class="carousel">
+    <div>
+      <img src="https://s26.postimg.org/labezuy7d/image.jpg" alt="js" />
+    </div>
 
-  <div>
-    <img src="https://s26.postimg.org/qzrnk64dl/image.jpg" alt="java" />
-  </div>
+    <div>
+      <img src="https://s26.postimg.org/qzrnk64dl/image.jpg" alt="java" />
+    </div>
 
-  <div>
-    <img src="https://s26.postimg.org/uxex9kr6x/image.jpg" alt="python"/>
-  </div>
-  
-  <div>
-    <img src="https://s26.postimg.org/oas9ge9ih/image.jpg" alt="kotlin" />
+    <div>
+      <img src="https://s26.postimg.org/uxex9kr6x/image.jpg" alt="python"/>
+    </div>
+    
+    <div>
+      <img src="https://s26.postimg.org/oas9ge9ih/image.jpg" alt="kotlin" />
+    </div>
   </div>
 </div>
 
 <script>
   $('.carousel').slick({
-    // dots: true,
     infinite: true,
     speed: 400,
     slidesToShow: 1,

9193cff85ed29404db06742d848d4e08828dbe1c
FelipePDS <felipepdasilva66@gmail.com>
2021-10-14 17:13:54 -0300

    fix: carousel layout

--- components/carousel-about-component.php
+++ components/carousel-about-component.php
@@ -1,59 +1,27 @@
-<!-- <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Compre e venda seus tênis de StreetWear aqui!</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>A confiança de sempre vender produtos originais</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Feito para resellers</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div> -->
-
-
 <div class="carousel-wrapper">
+  <button class="button-icon button-prev-icon">
+    <i class="fa fa-chevron-left"></i>
+  </button>
+  
   <div id="carousel-about" class="carousel">
     <div>
-    <label>Compre e venda seus tênis de StreetWear aqui!</label> 
+      <label>Compre e venda seus tênis de StreetWear aqui!</label> 
     </div>
 
     <div>
-    <label>    A confiança de sempre vender produtos originais </label>
+      <label>A confiança de sempre vender produtos originais</label>
     </div>
 
     <div>
-    <label>Feito para resellers </label>
+      <label>Feito para resellers </label>
     </div>
 
     <div>
-    <label>Porque seu estilo é nossa prioridade</label>
+      <label>Porque seu estilo é nossa prioridade</label>
     </div>
   </div>
+
+  <button class="button-icon button-next-icon">
+    <i class="fa fa-chevron-right"></i>
+  </button>
 </div>
